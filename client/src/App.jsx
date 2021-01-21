8ac064809d596d59e1fb20d0e14d0f103392ee63
Andre <vtavares.andre@gmail.com>
2021-01-20 23:24:31 -0300

    add bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 
 const App = () => {
   return (
     <div>
-      <h1>its meeee caraio</h1>
-      is meee
-      
+
+      <h1>he</h1>
+      <Container>
+        <h1>here</h1>
+
+      </Container>
+      <Header></Header>
+      <Footer></Footer>
     </div>
   )
 }
