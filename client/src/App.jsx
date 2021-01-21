4e2a0a6e73adac8bdcebec2552ddf22023603919
Andre <vtavares.andre@gmail.com>
2021-01-21 18:00:12 -0300

    navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import Footer from './components/Footer'
 import Header from './components/Header'
 
 
 const App = () => {
   return (
-    <div>
+    <>
 
-      <h1>he</h1>
+    <Header/>
+    <main className='py-3'>
+
       <Container>
+        <Row>
         <h1>here</h1>
+          
+        </Row>
 
       </Container>
-      <Header></Header>
-      <Footer></Footer>
-    </div>
+    </main>
+      <Footer/>
+
+    </>
   )
 }
 
