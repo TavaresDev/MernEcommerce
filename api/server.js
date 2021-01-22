323dc447b444d6aa3f225e0ffe907152174a158f
Andre <vtavares.andre@gmail.com>
2021-01-21 23:01:35 -0300

    nodemon+concurrently

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const { report } = require('process')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express()
 
@@ -15,4 +17,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('server on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Always watching... Running in ${process.env.NODE_ENV} on port ${PORT}`))
