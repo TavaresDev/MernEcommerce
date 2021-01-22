ee928268567801dbf1dfc2706f7508cc269bd632
Andre <vtavares.andre@gmail.com>
2021-01-21 22:22:41 -0300

    fech data with axios from backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Col, Row } from 'react-bootstrap'
-import products from '../products.js'
+// import products from '../products.js'/
 import Product from '../components/Product'
+import axios from 'axios'
 
 
 const Home = () => {
+    const [products, setProducts] = useState([])
+    // just diferent sintax from .get .then
+    useEffect(() => {
+        const fetchProducts = async () => {
+            const {data} = await axios.get('/api/products')
+            setProducts(data)
+
+        }
+        fetchProducts()
+    }, [])
+
     return (
         <>
         <h1>Latest products</h1>
