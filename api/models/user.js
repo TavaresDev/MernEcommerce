08701b6aab207daa4a97e5cbca69688e488a45f7
Andre <vtavares.andre@gmail.com>
2021-01-29 00:35:46 -0300

    add models

---
+++ api/models/user.js
@@ -0,0 +1,85 @@
+import mongoose from 'mongoose'
+// const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    dropDups: true,
+    validate: [
+      {
+        // value will be the email the user
+        // is attempting to store
+        validator: async function (value) {
+          const emailCount = await this.model('User')
+            .countDocuments({ email: value });
+          return emailCount === 0;
+        },
+        message: props => `Please try a different username/password combination.`
+      },
+      {
+        validator: function (value) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value.toLowerCase());
+        },
+        message: props => `Please ensure your email address is in the correct format. The email you entered was ${props.value}`
+      },
+      {
+        validator: function (value) {
+          return this.emailConfirmation === value;
+        },
+        message: props => `Your email and email confirmation must match.`
+      }
+    ]
+  },
+  password: {
+    type: String,
+    required :true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+// UserSchema.virtual('emailConfirmation')
+// .get(function () {
+//   return this._emailConfirmation;
+// })
+// .set(function (value) {
+//   this._emailConfirmation = value;
+// });
+
+// UserSchema.virtual('password')
+// .get(function () {
+//   return this._password;
+// })
+// .set(function (value) {
+//   console.log(value, this.passwordConfirmation, this);
+//   if (value !== this.passwordConfirmation) {
+//     this.invalidate('password', 'Password and password confirmation must match');
+//   }
+//   this._password = value;
+// });
+
+// UserSchema.virtual('passwordConfirmation')
+// .get(function () {
+//   return this._passwordConfirmation;
+// })
+// .set(function (value) {
+//   this._passwordConfirmation = value;
+// });
+
+// UserSchema.plugin(passportLocalMongoose, {
+//   usernameField: 'email'
+// });
+
+const User = mongoose.model('user', UserSchema)
+export default User
