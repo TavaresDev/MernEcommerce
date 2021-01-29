a3e853f1eba435165a79c7fd6d1fcd87b6c6b2df
Andre <vtavares.andre@gmail.com>
2021-01-29 14:04:06 -0300

    add reducers

--- api/server.js
+++ api/server.js
@@ -2,24 +2,22 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import cors from 'cors'
+
+import productRoutes from './routes/productRoutes.js'
+
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("APi is runing")
 })
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
+
 
 const PORT = process.env.PORT || 5000
 
