d71b3db846fb6b41950b347f510837566017d034
Andre <vtavares.andre@gmail.com>
2021-02-04 17:00:05 -0300

    cart front end and logic

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const {data} = await axios.get (`/api/products/${id}`)
-
     dispatch({
         type: CART_ADD_ITEM,
         payload: {
@@ -17,6 +16,16 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             qty,
         },
     })
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
+
+}
+export const removeFromCart = (id) =>  (dispatch, getState) => {
+
+    dispatch({
+        type: CART_REMOVE_ITEM,
+        payload: id,
+    })
+    
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 
 }
