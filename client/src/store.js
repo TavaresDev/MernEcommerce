7354afc402b1693cbad824469546108412d0b7d4
Andre <vtavares.andre@gmail.com>
2021-02-08 05:42:43 -0300

    pay order action

--- client/src/store.js
+++ client/src/store.js
@@ -4,7 +4,7 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import {productDetailsReducer, productListReducer} from './reducers/productReducers.js'
 import {cartReducer} from './reducers/cartReducers.js'
 import {userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer} from './reducers/userReducers.js'
-import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducers.js'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers.js'
 
 
 const reducer = combineReducers({
@@ -16,7 +16,8 @@ const reducer = combineReducers({
     userDetails : userDetailsReducer,
     userUpdateProfile : userUpdateProfileReducer,
     orderCreate : orderCreateReducer,
-    orderDetails: orderDetailsReducer
+    orderDetails: orderDetailsReducer,
+    orderPay : orderPayReducer
     
 })
 
